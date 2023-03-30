71f337a43d9d1ff0d91695f22608c97d873d9634
Jackie <4087635+gersteinj@users.noreply.github.com>
2023-03-30 19:24:53 -0400

    basic functionality for overlay

---
+++ overlay.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ws = new WebSocket('ws://localhost:8001');
+    const weightClass = document.getElementById('weightClass');
+    const redBot = document.getElementById('redBot');
+    const blueBot = document.getElementById('blueBot');
+
+    ws.onmessage = ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+            case 'users':
+                console.log(`A user connected or disconnected. There are currently ${event.count} users.`);
+                userCount.innerText = event.count;
+                break;
+            case 'match':
+                const match = event.value;
+                console.log(match);
+                weightClass.innerText = match.weightClass;
+                redBot.innerText = match.redBot;
+                blueBot.innerText = match.blueBot;
+                break;
+            default:
+                console.log(`I'm not sure what to do with ${event.type} events.`);
+        }
+    }
+})
